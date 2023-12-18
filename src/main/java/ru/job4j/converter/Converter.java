33d7499a3a151874737fbac548e6996194d27d3d
leha5345 <leha5345@gmail.com>
2023-12-18 17:53:54 +0300

    0. Что такое тестирование [#159431 #467165]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,30 +2,32 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+        float result = value / 70;
+        return result;
     }
-
     public static float rubleToDollar(float value) {
-        float rsl = value / 60;
-        return rsl;
+        float result = value / 60;
+        return result;
     }
-
     public static void main(String[] args) {
-        float in = 140;
-        double expected = 2;
-        float out = Converter.rubleToEuro(in);
-        boolean passed = expected == out;
-        System.out.println("140 rubles are  2 euro. Test result : " + passed);
+        float inputEuro = 140F;
+        float expectedToEuro = 2F;
+        float outputEuro = rubleToEuro(inputEuro);
+        boolean passedToEuro = expectedToEuro == outputEuro;
+        if (passedToEuro) {
+            System.out.println(inputEuro + " rubles are " + outputEuro + " Test result: " + passedToEuro);
+        } else {
+            System.out.println("Test failed");
+        }
 
-        in = 140;
-        expected = 2.3333333;
-        out = Converter.rubleToDollar(in);
-        passed = expected == out;
-        System.out.println("140 rubles are 2.3333333 dollars. Test result : " + passed);
-        float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("140 rubels are " + dollar + " dollars.");
+        float inputDollar = 180F;
+        float expectedToDollar = 3F;
+        float outputDollar = rubleToDollar(inputDollar);
+        boolean passedToDollar = expectedToDollar == outputDollar;
+        if (passedToDollar) {
+            System.out.println(inputDollar + " rubles are " + outputDollar + " Test result: " + passedToDollar);
+        } else {
+            System.out.println("Test failed");
+        }
     }
 }
