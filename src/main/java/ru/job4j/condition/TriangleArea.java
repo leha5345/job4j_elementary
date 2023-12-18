f35e7492043ae3f9f7ccb857652c404b0db67398
leha5345 <leha5345@gmail.com>
2023-12-19 00:06:41 +0300

    9. Площадь треугольника. [#175474 #467119]

--- src/main/java/ru/job4j/condition/TriangleArea.java
+++ src/main/java/ru/job4j/condition/TriangleArea.java
@@ -9,6 +9,6 @@ public class TriangleArea {
 
     public static void main(String[] args) {
         double result = TriangleArea.area(2, 2, 2);
-        System.out.println("area (2, 2, 2) = " + result);
+        System.out.println("area  (2, 2, 2) = " + result);
     }
 }
