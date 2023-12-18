30e4ecb95de56ebc19b31716391c5411b336a0bc
leha5345 <leha5345@gmail.com>
2023-12-19 00:02:02 +0300

    7. Расстояние между точками в системе координат [#188 #467111]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,9 +1,4 @@
 package ru.job4j.condition;
-//1. Создайте класс ru.job4j.condition.Point. Допишите метод distance.
-//2. Доработайте метод main. Нужно добавить в него вызов метода distance с различными аргументами.
-//3. Загрузите код в репозиторий. Оставьте ссылку на коммит.
-//double result = Math.pow(3, 2); // Возвести 3 в квадрат
-//double result = Math.sqrt(16); // Вычислить квадратный корень из 16
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
